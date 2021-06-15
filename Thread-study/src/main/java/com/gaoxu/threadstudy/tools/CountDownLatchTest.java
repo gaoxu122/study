4170bb3ed78a0cddf6d998db213ac6ce4150b910
gaoxu <1290043773@qq.com>
2021-06-15 14:48:07 +0800

    修改代码

--- Thread-study/src/main/java/com/gaoxu/threadstudy/tools/CountDownLatchTest.java
+++ Thread-study/src/main/java/com/gaoxu/threadstudy/tools/CountDownLatchTest.java
@@ -1,7 +1,11 @@
 package com.gaoxu.threadstudy.tools;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.LockSupport;
 
+/**
+ * 多个线程同时执行 模拟并发场景
+ */
 public class CountDownLatchTest {
 
     public static void main(String[] args) throws InterruptedException {
@@ -15,6 +19,9 @@ public class CountDownLatchTest {
                 try {
                     countDownLatch.await();
                     System.out.println(System.currentTimeMillis());
+
+                    LockSupport.park();
+                    System.out.println("111111111111111111");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
--- Thread-study/src/main/java/com/gaoxu/threadstudy/tools/SemaphoreTest.java
+++ Thread-study/src/main/java/com/gaoxu/threadstudy/tools/SemaphoreTest.java
@@ -1,4 +1,18 @@
 package com.gaoxu.threadstudy.tools;
 
+import lombok.Synchronized;
+
+import java.util.concurrent.locks.Lock;
+
 public class SemaphoreTest {
+
+
+    public static void main(String[] args) {
+
+        StringBuffer stringBuffer = new StringBuffer();
+    }
+
+    public void testSync() {
+
+    }
 }
